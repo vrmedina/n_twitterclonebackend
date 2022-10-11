6c8d7689089991d3ea9becd0789973becf7ecb1f
Victor Medina <vrmm1998@gmail.com>
2022-10-10 19:06:36 -0500

    Implementing routes and models for users

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const routes = require("./routes/userRoute")
 
-// Connect to MongoDB database
 mongoose
 	.connect("mongodb://localhost:27017/twitter", { useNewUrlParser: true })
 	.then(() => {
 		const app = express()
+		app.use("/api", routes) // new
 
 		app.listen(5000, () => {
 			console.log("Server has started!")
